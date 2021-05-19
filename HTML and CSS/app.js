0e8119fa7a68c0bdebf9e456d66a8c5c8841a1ad
Mohamoud Torki Aljadan <mohamoud.aljadan@b2wdigital.com>
2021-05-19 19:34:10 -0300

    refactor: - adding draggable ability, by adding edit/save button - adding list to view all the markers, - adding ability to click on the list item and mark it with animation on the map.

--- HTML and CSS/app.js
+++ HTML and CSS/app.js
@@ -1,7 +1,26 @@
-var customIcon = L.icon({
-    iconUrl: 'https://png2.cleanpng.com/sh/231d7ed31f29fc6f8a8921d63dd49ec7/L0KzQYm3UsA0N5R1fZH0aYP2gLBuTf9nbppofZ9sbHnzPbL5lL14d6NwgdDwLYTydLLCTfNtcaFmiuZ8LUXkR4bqVvVkO5JrSdU8Lke1QIe9VsE5OWY2T6gBOEC6RoO7UcQveJ9s/kisspng-office-clip-art-working-today-cliparts-5a75c6ec3af1c3.7206661815176680762414.png',
-    // shadowUrl: 'leaf-shadow.png',
+var editBttn = document.getElementById('edit-btn')
+var oldPosition = {}
+var isDraggable = false
 
+
+var newIcon = L.divIcon({
+    className: 'location-pin',
+    html: `
+        <img src='./green-laptop.png'>
+        <div class="pulse"></div>
+        `,
+    // iconSize: [25, 25],//size being set in css.
+    iconAnchor: [10, 10],
+
+    shadowSize: [20, 20], // size of the shadow
+    shadowAnchor: [13, 13],  // the same for the shadow
+    popupAnchor: [3, -12] // point from which the popup should open relative to the iconAnchor
+});
+
+
+var customIcon = L.icon({
+    // shadowUrl: 'leaf-shadow.png',
+    iconUrl: 'green-laptop.png',
     iconSize: [25, 25], // size of the icon
     shadowSize: [20, 20], // size of the shadow
     iconAnchor: [10, 10], // point of the icon which will correspond to marker's location
@@ -12,15 +31,18 @@ var customIcon = L.icon({
 var map = L.map('image-map', {
     minZoom: 1,
     maxZoom: 5,
-    center: [0, 0],
+    center: [-456, -440],
     zoom: 3,
     crs: L.CRS.Simple,
+    doubleClickZoom: false,// disable dbClick zoom
 });
 
 // dimensões na imagem com url dela
-var w = 2000,
-    h = 1500,
-    url = 'http://kempe.net/images/newspaper-big.jpg',
+var w = 2000, // careful with these numbers, as they change the position of the desk on the picture. once they are set and 
+    h = 1500  // the markers were added , these numbers shouldnt be modified.
+// var w = 3350,
+// h = 2550,
+url = 'http://kempe.net/images/newspaper-big.jpg',
     f1Url = 'f1.png';
 
 
@@ -41,33 +63,131 @@ map.setMaxBounds(bounds);
 console.log(reservationList);
 
 //Hadnel on right click functions TODO: MOVE THIS LATER
-map.on('contextmenu', onMapClick);
+map.on('contextmenu', onMapRightClick);
 
-
-// reservationList.forEach(element => {
-
-//     L.marker([element.lat, element.lng], { icon: customIcon }).addTo(map).bindPopup(
-//         '<p>Quer reservar essa mesa?</p> <button type="button"class="btn btn-primary btn-sm">Sim</button> <button type="button"class="btn btn-danger btn-sm">Não</button>'
-//     );
-//     L.circle([element.lat, element.lng], 1).addTo(map);
-// });
+map.on('click', onMapClick)
 
 // how geoJSON work https://leafletjs.com/examples/geojson/
-const geoLayer = L.geoJSON(reservationList, {
-    onEachFeature: forEachItem,
-    pointToLayer: function (feature, latlng) {
-        return L.marker(latlng, { icon: customIcon });
-    }
-});
-geoLayer.addTo(map)
+// this instance (variable) is only being used in the Edit button so we can remove 
+// the layer from the map and generate/add it again 
+var geoJson = generateMarkers(false);
 
+
+
+
+// i dont know what this does?
 Number.prototype.between = function (a, b) {
     var min = Math.min.apply(Math, [a, b]),
         max = Math.max.apply(Math, [a, b]);
     return this > min && this < max;
 };
 
+editBttn.addEventListener('click', OnEditClick)
+
+function generateMarkers(draggable, id = -1, newIcon) {
+
+    const geoLayer = L.geoJSON(reservationList, {
+        onEachFeature: forEachItem,
+        pointToLayer: function (feature, latlng) {
+            var icon = customIcon
+            if (id != -1) {
+                if (feature.properties.id === id) {
+                    console.log('found id checking feature, id:', id);
+                    console.log(feature);
+                    icon = newIcon
+                }
+            }
+            var marker = L.marker(latlng, { icon: icon, draggable: draggable });
+            marker.on('moveend', onMarkerDragged); // moveEnd is same as dragEnd (the later happens multiple times dont know why)
+            // just testing dbClick, (problem double click also make zoom if it was not on the marker)
+            marker.on('dblclick', onDoubleClickMarker)
+            //this event is ony to record the oldPosition in case if the last position is out of the map
+            // to reset the markert to the old position
+            marker.on('dragstart', function (e) {
+                oldPosition = e.target._latlng
+            })
+
+            return marker;
+        }
+    });
+    geoLayer.addTo(map);
+    updateList()
+    return geoLayer;
+}
+
+function OnEditClick(e) {
+    e.preventDefault();
+
+    // to disable/enable draggable on each marker in the map layer, 
+    // need to remove them and create them with draggable false/true. this is what worked with me.
+    if (editBttn.innerText == 'Edit') {
+
+        editBttn.innerText = 'Save'
+        map.removeLayer(geoJson)
+        isDraggable = !isDraggable
+        geoJson = generateMarkers(isDraggable)
+
+    } else {
+
+        editBttn.innerText = 'Edit'
+        map.removeLayer(geoJson)
+        isDraggable = !isDraggable
+        geoJson = generateMarkers(isDraggable)
+
+    }
+}
+
+function onDoubleClickMarker(e) {
+    console.log('double clicked');
+    console.log(e.target.dragging);
+    console.log(e);
+
+}
 function onMapClick(e) {
+    console.log('map clicked');
+    console.log('print target');
+    console.log(e.target);
+};
+
+function onMarkerDragged(event) {
+
+    var marker = event.target;
+    var position = marker.getLatLng();
+
+    if (event.target._latlng.lng.between(bounds._southWest.lng, bounds._northEast.lng) && event.target._latlng.lat.between(bounds._southWest.lat, bounds._northEast.lat)) {
+
+        console.log('setting new position');
+        marker.setLatLng(new L.LatLng(position.lat, position.lng));
+
+        // saving the coordinates in memory, later When click on Save button can be persisted to DB.
+        saveNewCoordinates(position, marker.feature)
+    } else {
+        //falling back to the old position of the market because the markert was dragged out of the layer bounds.
+        marker.setLatLng(new L.LatLng(oldPosition.lat, oldPosition.lng));
+        console.log('outside, falling back');
+
+    }
+}
+
+// update with new coordinates 
+function saveNewCoordinates(position, feature) {
+    console.log('save coordinates for :', feature.properties.name, ', id:', feature.properties.id);
+    var deskId = feature.properties.id
+    console.log(feature);
+    reservationList.map(item => {
+        if (item.properties.id === deskId) {
+            console.log('item changed : ', item.properties.id);
+            item.geometry.coordinates[0] = position.lng
+            item.geometry.coordinates[1] = position.lat
+        }
+    })
+}
+
+function onMapRightClick(e) {
+    // deactivate right click if not in edit mode
+    if (!isDraggable) {
+        return
+    }
     var mapWidth = map._container.offsetWidth;
     var mapHeight = map._container.offsetHeight;
     var locationX = e.containerPoint.x * w / mapWidth;
@@ -84,49 +204,27 @@ function onMapClick(e) {
             "type": "Feature",
             "geometry": {
                 "type": "Point",
-                "coordinates": [e.latlng.lat, e.latlng.lng]
+                "coordinates": [e.latlng.lng, e.latlng.lat]
             },
             "properties": {
-                "mesa": reservationList.length + 1,
+                "id": reservationList.length + 1,
                 "name": "Destk " + reservationList + 1,
-                "name": "person name ",
-                "phone": "23 2323 2323"
+                "name": "empty",
+                "phone": "emtpy"
             }
         }
+        // only need to add the coordinates and let generateMarkers do the work.
         reservationList.push(obj);
+        geoJson = generateMarkers(true)
 
-        L.Circle.include({
-            contains: function (latLng) {
-                return this.getLatLng().distanceTo(latLng) < this.getRadius();
-            }
-        });
-
-        var greenIcon = L.icon({
-            iconUrl: 'https://png2.cleanpng.com/sh/231d7ed31f29fc6f8a8921d63dd49ec7/L0KzQYm3UsA0N5R1fZH0aYP2gLBuTf9nbppofZ9sbHnzPbL5lL14d6NwgdDwLYTydLLCTfNtcaFmiuZ8LUXkR4bqVvVkO5JrSdU8Lke1QIe9VsE5OWY2T6gBOEC6RoO7UcQveJ9s/kisspng-office-clip-art-working-today-cliparts-5a75c6ec3af1c3.7206661815176680762414.png',
-            // shadowUrl: '',
-
-            iconSize: [25, 25], // size of the icon
-            shadowSize: [20, 20], // size of the shadow
-            iconAnchor: [10, 10], // point of the icon which will correspond to marker's location
-            shadowAnchor: [13, 13],  // the same for the shadow
-            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-        });
-
-        L.marker([e.latlng.lat, e.latlng.lng], { icon: greenIcon }).addTo(map).bindPopup('<p>Quer reservar essa mesa?</p> <button type="button"class="btn btn-primary btn-sm">Sim</button> <button type="button"class="btn btn-danger btn-sm">Não</button>');
-        // L.marker([e.latlng.lat, e.latlng.lng], {icon: redMarker}).addTo(map);
-        var circle = L.circle([e.latlng.lat, e.latlng.lng], 1).addTo(map);
     } else
         console.log("not okay")
 
-    //map.fitBounds(circle.getBounds());
     console.log(reservationList);
 
 }
 
 function forEachItem(feature, layer) {
-    // .bindPopup(
-    // '<p>Quer reservar essa mesa?</p> <button type="button"class="btn btn-primary btn-sm">Sim</button> <button type="button"class="btn btn-danger btn-sm">Não</button>'
-    // );
     layer.bindPopup(makePopupContent(feature), { closeButton: false, offset: L.point(0, -8) });
 }
 
@@ -143,4 +241,37 @@ function makePopupContent(desk) {
         <button type="button"class="btn btn-primary btn-sm">Sim</button> 
         <button type="button"class="btn btn-danger btn-sm">Não</button>
     `;
+}
+
+function updateList() {
+    const ul = document.querySelector('.list');
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+    }
+    reservationList.forEach((desk) => {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        const a = document.createElement('a');
+        const p = document.createElement('p');
+        a.addEventListener('click', () => {
+            animateMarker(desk);
+        });
+        div.classList.add('desk-item');
+        a.innerText = desk.properties.name;
+        a.href = '#';
+        p.innerText = desk.properties.occupation;
+
+        div.appendChild(a);
+        div.appendChild(p);
+        li.appendChild(div);
+        ul.appendChild(li);
+    });
+}
+
+
+function animateMarker(desk) {
+    const id = desk.properties.id
+    map.removeLayer(geoJson)
+    // always assign it to this variable, to be able to remove it later. (otherwise strange things start happening, overlapping layers)
+    geoJson = generateMarkers(false, id, newIcon)
 }
--- HTML and CSS/reservation_list.js
+++ HTML and CSS/reservation_list.js
@@ -3,10 +3,10 @@ const reservationList = [
         "type": "Feature",
         "geometry": {
             "type": "Point",
-            "coordinates": [55.625, -28.625]
+            "coordinates": [55.625, -28.25]
         },
         "properties": {
-            "mesa": 0,
+            "id": 0,
             "name": "lucas",
             "occupation": "programmer",
             "phone": "23 2323 2323"
@@ -16,10 +16,10 @@ const reservationList = [
         "type": "Feature",
         "geometry": {
             "type": "Point",
-            "coordinates": [63.09375, -28.4375]
+            "coordinates": [63.09375, -28.25]
         },
         "properties": {
-            "mesa": 1,
+            "id": 1,
             "name": "pedro",
             "occupation": "programmer",
             "phone": "23 2323 0009"
@@ -32,7 +32,7 @@ const reservationList = [
             "coordinates": [70.65625, -28.25]
         },
         "properties": {
-            "mesa": 1,
+            "id": 2,
             "name": "khaled",
             "occupation": "programmer",
             "phone": "23 2323 0088"
